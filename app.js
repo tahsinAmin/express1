4c00a73c59c2d704c963f5d384257ae3e2b7d4dc
Muhammad Tahsin Amin <muhammadtahsinamin@gmail.com>
2021-12-29 18:32:36 +0600

    Repo Created

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+
+// Middleware
+app.use(express.static("public"));
+
+// import Router
+const weatherRoute = require("./routes/weather");
+
+// use View Engine
+app.set("view engine", "ejs");
+
+// Middleware route
+app.use("/", weatherRoute);
+const PORT = process.env.PORT || 5005;
+
+app.listen(PORT, () => console.log(`Server starting at port ${PORT}`));
+
+// https://www.youtube.com/watch?v=PozEoga90r8
