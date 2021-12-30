2bd174e425c39fb766a297c155de3a786915b176
Muhammad Tahsin Amin <muhammadtahsinamin@gmail.com>
2021-12-30 11:19:17 +0600

    Show city name as a heading in the index file

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 
 - [x] 1. Reading the parameter
 - [x] 2. Axios Data fetct with city name using axios
-- [ ] 3. Show city name as a heading in the index fule
+- [x] 3. Able to retrieve individual values
+- [x] 4. Show city name as a heading in the index file
--- routes/weather.js
+++ routes/weather.js
@@ -3,18 +3,23 @@ const axios = require("axios");
 // const path = require("path");
 // require("dotenv").config();
 
-router.get("/", (req, res) => {
-  res.render("index");
-});
+// router.get("/", (req, res) => {
+//   res.render("index");
+// });
 
 router.get("/all/:country/:city", ({ params }, res) => {
   const { country, city } = params;
+  console.log(country, city);
   const url_api = `http://api.weatherapi.com/v1/current.json?key=e6a73467a3e94aa184c122435212812&q=${city}&aqi=no`;
-  axios
-    .get(url_api, async (res) => {
-      await res.json();
-    })
-    .then((data) => console.log(data));
+  axios.get(url_api).then((response) => {
+    var jsonObject = JSON.stringify(response.data);
+    var object = JSON.parse(jsonObject);
+    console.log(object.location.name);
+    res.render("index", {
+      city: object.location.name,
+      country: object.location.country,
+    });
+  });
 });
 
 // res.render("index", {
